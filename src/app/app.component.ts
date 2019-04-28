4ca402af1beaa148c7ef6729b902503af5c5eb60
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2019-04-28 15:09:29 -0700

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 //import * as firebase from 'firebase';
 
-const config = {
-  apiKey: "AIzaSyClbYhiX-oQbxs8UJzG6rbbmePYDz0GK80",
-  authDomain: "rocket-build.firebaseapp.com",
-  databaseURL: "https://rocket-build.firebaseio.com",
-  projectId: "rocket-build",
-  storageBucket: "rocket-build.appspot.com",
-  messagingSenderId: "269786933365"
-};
 
 @Component({
   selector: 'app-root',
