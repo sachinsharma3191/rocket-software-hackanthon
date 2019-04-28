044d13a60982bf893d1bfabd7882c62fbac767b5
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2019-04-27 21:04:43 -0700

    Üi Development

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rocket-build';
-services = ["Discounted Food","Medical Service","Shelter"];
-  
-onClickMe(){
-    alert();
-   
-  }
+ 
 }
